fffb0c3c229c04970971e31247dc533560776072
Rajat Vajpayee <rajatvajpayee@gmail.com>
2019-05-20 09:22:32 +0530

    Update and rename Voice_Search.py to google_voices_search.py

--- google_voices_search.py
+++ google_voices_search.py
@@ -5,16 +5,19 @@ import webbrowser
 ## Voice_Recognition
 r = sr.Recognizer()
 with sr.Microphone() as source :    
-    print('Speak:')
+    print('Speak Now:')
     audio = r.listen(source)
 
     try:
+        print('Listening')
         text = r.recognize_google(audio)
         print('You said: {}'.format(text))
+        txt = text;
+        if txt.split()[0] == 'Google'  :
+            new = 2 
+            url = "http://google.com/?#q=";
+            webbrowser.open(url+text,new = new);
+        else:
+            print('Try adding google in Start');
     except:
         print('Sorry could not understand')
-        
-## Opening URL
-new = 1 
-url = "http://google.com/?#q=";
-webbrowser.open(url+text,new = new);
